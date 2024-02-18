b5bfab6017dceab92940093fecc9d7f3c7e4ad8d
Daniel Odvarka <odvarkaadaniel@gmail.com>
2024-02-18 15:02:16 +0100

    Initial structure of the code

--- hashring.go
+++ hashring.go
@@ -1,13 +1,25 @@
 package hashring
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+)
+
+// TODO: Make these values sensible.
+const (
+	DefaultPartitionCount    = 13
+	DefaultReplicationFactor = 20
+)
+
+type HashFn func([]byte) uint64
 
 type Config struct {
-	Hasher interface{}
+	Hasher HashFn
 
-	// TODO: rename?
-	//
+	// ReplicationFactor determines the number of virtual nodes we
+	// put into the hash ring.
 	ReplicationFactor int
+	PartitionCount    int
 }
 
 type Bucket interface {
@@ -17,13 +29,150 @@ type Bucket interface {
 type HashRing struct {
 	mu sync.RWMutex
 
-	config Config
+	hasher HashFn
+	// sortedSet         []uint64
+	partitionCount    uint64
+	ReplicationFactor uint64
 
-	// Maybe just func func(x []byte) uint64
-	// Or an actual interface
-	hasher interface{}
+	buckets    map[string]Bucket
+	partitions map[int]Bucket
+	ring       map[uint64]Bucket
 }
 
-func New(config *Config) *HashRing {
-	return nil
+func New(config Config, buckets []Bucket) *HashRing {
+	if config.Hasher == nil {
+		panic("hasher can not be nil")
+	}
+
+	hr := &HashRing{
+		hasher:     config.Hasher,
+		ring:       make(map[uint64]Bucket),
+		buckets:    make(map[string]Bucket),
+		partitions: make(map[int]Bucket),
+	}
+
+	if config.PartitionCount == 0 {
+		hr.partitionCount = DefaultPartitionCount
+	}
+
+	if config.ReplicationFactor == 0 {
+		hr.ReplicationFactor = DefaultReplicationFactor
+	}
+
+	for _, b := range buckets {
+		hr.add(b)
+	}
+
+	if len(hr.buckets) > 0 {
+		hr.distibute()
+	}
+
+	return hr
+}
+
+func (hr *HashRing) distibute() {
+
+}
+
+// Add adds a new bucket to the consistent hash ring.
+func (hr *HashRing) Add(bucket Bucket) {
+	hr.add(bucket)
+}
+
+func (hr *HashRing) add(bucket Bucket) {
+	hr.mu.Lock()
+	defer hr.mu.Unlock()
+
+	if _, ok := hr.buckets[bucket.String()]; ok {
+		// TODO: Log that we already have this member?
+		return
+	}
+
+	for i := uint64(0); i < hr.ReplicationFactor; i++ {
+		key := fmt.Sprintf("%s%d", bucket.String(), i)
+		hash := hr.hasher([]byte(key))
+
+		hr.ring[hash] = bucket
+	}
+
+	hr.buckets[bucket.String()] = bucket
+
+	hr.distibute()
+}
+
+// Remove removes a bucket from the consistent hash ring.
+func (hr *HashRing) Remove(key string) {
+	hr.remove(key)
+}
+
+func (hr *HashRing) remove(key string) {
+	hr.mu.Lock()
+	defer hr.mu.Unlock()
+
+	if _, ok := hr.buckets[key]; !ok {
+		// TODO: Log that the member doesn't exist?
+		return
+	}
+
+	for i := uint64(0); i < hr.ReplicationFactor; i++ {
+		key := fmt.Sprintf("%s%d", key, i)
+		hash := hr.hasher([]byte(key))
+
+		delete(hr.ring, hash)
+	}
+
+	delete(hr.buckets, key)
+
+	hr.distibute()
+}
+
+func (hr *HashRing) Buckets() []Bucket {
+	hr.mu.RLock()
+	defer hr.mu.RUnlock()
+
+	return hr.getBuckets()
+}
+
+func (hr *HashRing) getBuckets() []Bucket {
+	buckets := []Bucket{}
+
+	for _, b := range hr.buckets {
+		buckets = append(buckets, b)
+	}
+
+	return buckets
+}
+
+// TODO: Implement the functionality.
+func (hr *HashRing) GetClosestN(key string, n int) ([]Bucket, error) {
+	return nil, nil
+}
+
+// GetPartitionID gets a partition id for a given key in the ring.
+func (hr *HashRing) GetPartitionID(key string) int {
+	return hr.getPartitionID(key)
+}
+
+func (hr *HashRing) getPartitionID(key string) int {
+	hr.mu.RLock()
+	defer hr.mu.RUnlock()
+
+	hash := hr.hasher([]byte(key))
+
+	return int(hash % hr.partitionCount)
+}
+
+func (hr *HashRing) GetPartitionBucket(id int) Bucket {
+	return hr.getPartitionBucket(id)
+}
+
+func (hr *HashRing) getPartitionBucket(id int) Bucket {
+	hr.mu.RLock()
+	defer hr.mu.RUnlock()
+
+	if bucket, ok := hr.partitions[id]; !ok {
+		return nil
+	} else {
+		return bucket
+	}
 }
